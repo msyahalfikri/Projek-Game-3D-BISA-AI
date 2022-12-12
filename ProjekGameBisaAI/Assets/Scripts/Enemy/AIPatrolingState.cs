df742a2cd6a798d0ca318026ab2e55c5fadba8ec
MuhammadSyahAlFikri_056 <fikriibabangdaffa@gmail.com>
2022-12-12 16:56:52 +0700

    bug fixing

--- ProjekGameBisaAI/Assets/Scripts/Enemy/AIPatrolingState.cs
+++ ProjekGameBisaAI/Assets/Scripts/Enemy/AIPatrolingState.cs
@@ -14,19 +14,19 @@ public class AIPatrolingState : AIState
     }
     public void Update(AIAgent agent)
     {
-        // if (!agent.navMeshAgent.hasPath)
-        // {
-        WorldBounds worldBounds = GameObject.FindObjectOfType<WorldBounds>();
-        Vector3 min = worldBounds.min.position;
-        Vector3 max = worldBounds.max.position;
-        Vector3 randomPosition = new Vector3(
-           Random.Range(min.x, max.x),
-            Random.Range(min.y, max.y),
-              Random.Range(min.z, max.z)
-        );
+        if (!agent.navMeshAgent.hasPath)
+        {
+            WorldBounds worldBounds = GameObject.FindObjectOfType<WorldBounds>();
+            Vector3 min = worldBounds.min.position;
+            Vector3 max = worldBounds.max.position;
+            Vector3 randomPosition = new Vector3(
+               Random.Range(min.x, max.x),
+                Random.Range(min.y, max.y),
+                  Random.Range(min.z, max.z)
+            );
 
-        agent.navMeshAgent.destination = randomPosition;
-        // }
+            agent.navMeshAgent.destination = randomPosition;
+        }
     }
     public void Exit(AIAgent agent)
     {
